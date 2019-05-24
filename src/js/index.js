31eb1a8b78c202890ab15d05e5a8efdc0ea5d64d
Bessi <425782059@qq.com>
2019-05-24 17:35:21 +0800

    修改点击bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,16 @@
 
+
+
 (function () {
-    $('.ui-list,.ui-tiled, .ui-href').on('touchstart','li',function (e) {
-        if ($(this).data('href')) {
-            location.href = $(this).data('href');
+
+    // 点击li跳转到详情页
+    $('.ui-list, .ui-tiled, .ui-href').on('click', function(e){
+        console.log(e)
+        if ($(e.target).parents('li').data('href')) {
+            location.href = $(e.target).parents('li').data('href');
         }
-    });
+    })
+    
     $('.ui-header .ui-btn').click(function () {
         location.href = 'index.html';
     });
@@ -20,5 +26,43 @@
             'transition': 'transform 0.5s linear'
         })
     });
+
+ /* // 点击li跳转到详情页
+    $('.ui-list, .ui-tiled, .ui-href').on('tap', function(e){
+        console.log(e)
+        if ($(e.target).parents('li').data('href')) {
+            location.href = $(e.target).parents('li').data('href');
+        }
+    })
+    function tap(dom, callback){
+        if(!dom || typeof dom != "object"){
+            return;
+        }
+        var startTime,startX,startY;
+        dom.on('touchstart',function (e) {
+            // 判断是否只有一根手指
+            if(e.targetTouches.length > 1){
+                return;
+            }
+            startTime = Date.now();
+            startX = e.targetTouches[0].clientX;
+            startY = e.targetTouches[0].clientY;
+        });
+        dom.on('touchend',function (e) {
+            // 判断是否只有一根手指
+            if(e.changedTouches.length > 1){
+                return;
+            }
+            // 判断时间差异
+            if(Date.now() - startTime >150){
+                return;
+            }
+            var endX = e.changedTouches[0].clientX;
+            var endY = e.changedTouches[0].clientY;
+            if(Math.abs(endX - startX) < 6 && Math.abs(endY - startY) < 6){
+                callback && callback(e);
+            }
+        });
+    } */
 	
 })(window, undefined)
